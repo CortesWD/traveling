f96a729d8c9893c3cf65d31104ae0233504977ca
CortesWebDesign <cccp.designer@gmail.com>
2013-05-14 01:23:25 -0500

    Active video

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).on("ready", backvid);
 
 function backvid()
 {
-	$("background").append("<iframe src='?portrait=0&loop=1&title=0&byline=0&autoplay=1&hd_off=1' width='100%' height='100%' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> ");
+	$("#background").append("<iframe src='?portrait=0&loop=1&title=0&byline=0&autoplay=1&hd_off=1' width='100%' height='100%' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> ");
 	$("#offit").on("click", turnoff);
 };
 
