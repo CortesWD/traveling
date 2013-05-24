afa7b1fcf5b70e5ef664308f34ef2241a206982e
CortesWebDesign <cccp.designer@gmail.com>
2013-05-24 03:08:26 -0500

    Final

--- js/main.js
+++ js/main.js
@@ -7,21 +7,3 @@ function backvid()
 };
 
 
-$("#play").on("click", vid1);
-
-var background =
-{
-	background: "none"
-};
-
-var vidbta = function()
-{
-	$("#video").css(background);
-	$("#video").replaceWith("<iframe src='http://player.vimeo.com/video/57930404?portrait=0&loop=1&title=0&byline=0&autoplay=1&hd_off=1' width='800' height='400' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe");
-}
-
-function vid1()
-{
-	
-	$("#video").fadeOut(1000, vidbta);
-}
